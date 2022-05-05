ead34ae400b629b7b32f10d6eafd277fc6a92613
sxabc <2947261028@qq.com>
2022-05-05 20:29:32 +0800

    feat: 退出登录方案实现

--- src/modules/user.js
+++ src/modules/user.js
@@ -1,7 +1,11 @@
 import { login, getUserInfo } from '@/api/sys'
 import md5 from 'md5'
-import { setItem, getItem } from '@/utils/storage'
 import { TOKEN } from '@/constant'
+// 增加解构removeAllItem
+import { setItem, getItem, removeAllItem } from '@/utils/storage'
+// 增加 导入路由
+import router from '@/router'
+
 export default {
   namespaced: true,
   state: () => ({
@@ -41,6 +45,12 @@ export default {
       console.log(res)
       this.commit('user/setUserInfo', res)
       return res
+    },
+    logout() {
+      this.commit('user/setToken', '')
+      this.commit('user/setUserInfo', {})
+      removeAllItem()
+      router.push('/login')
     }
   }
 }
