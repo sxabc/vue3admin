8a6e1288181bfa53291a552586a87c4e89c61709
sxabc <2947261028@qq.com>
2022-05-05 20:47:59 +0800

    feat: 用户被动退出方案之主动处理

--- src/modules/user.js
+++ src/modules/user.js
@@ -5,6 +5,7 @@ import { TOKEN } from '@/constant'
 import { setItem, getItem, removeAllItem } from '@/utils/storage'
 // 增加 导入路由
 import router from '@/router'
+import { setTimeStamp } from '@/utils/auth'
 
 export default {
   namespaced: true,
@@ -31,6 +32,7 @@ export default {
           password: md5(password)
         })
           .then((data) => {
+            setTimeStamp()
             resolve(data)
             console.log(data)
             this.commit('user/setToken', data.data.token)
