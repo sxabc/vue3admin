347c88700c53399ec6ec8c72b669d45e3383459e
sxabc <2947261028@qq.com>
2022-05-05 20:52:22 +0800

    feat: 用户退出方案之被动处理

--- src/utils/request.js
+++ src/utils/request.js
@@ -21,8 +21,16 @@ service.interceptors.response.use(
       return Promise.reject(new Error(message))
     }
   },
-  (error) => {
-    // TODO: 将来处理 token 超时问题
+  error => {
+    // 处理 token 超时问题
+    if (
+      error.response &&
+      error.response.data &&
+      error.response.data.code === 401
+    ) {
+      // token超时
+      store.dispatch('user/logout')
+    }
     ElMessage.error(error.message) // 提示错误信息
     return Promise.reject(error)
   }
