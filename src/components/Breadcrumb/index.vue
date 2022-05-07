27f7bc379605bc59d388b13b709221d83e8563c0
sxabc <2947261028@qq.com>
2022-05-07 23:24:22 +0800

    perf: sidebar 与面包屑区域的国际化处理

--- src/components/Breadcrumb/index.vue
+++ src/components/Breadcrumb/index.vue
@@ -2,11 +2,15 @@
   <el-breadcrumb class="breadcrumb" separator="/">
     <transition-group name="breadcrumb">
       <el-breadcrumb-item v-for="(item, index) in breadcrumbData" :key="item.path">
-      <!-- 不可点击项 -->
-      <span v-if="index === breadcrumbData.length - 1" class="no-redirect">{{ item.meta.title }}</span>
-      <!-- 可点击项 -->
-      <a v-else class="redirect" @click.prevent="onLinkClick(item)">{{ item.meta.title }}</a>
-    </el-breadcrumb-item>
+        <!-- 不可点击项 -->
+        <span v-if="index === breadcrumbData.length - 1" class="no-redirect">
+          {{ generateTitle(item.meta.title) }}
+        </span>
+        <!-- 可点击项 -->
+        <a v-else class="redirect" @click.prevent="onLinkClick(item)">
+          {{ generateTitle(item.meta.title) }}
+        </a>
+      </el-breadcrumb-item>
     </transition-group>
   </el-breadcrumb>
 </template>
@@ -15,6 +19,7 @@
 import { ref, watch } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 import { useStore } from 'vuex'
+import { generateTitle } from '@/utils/i18n'
 
 const route = useRoute()
 // 生成数组数据
