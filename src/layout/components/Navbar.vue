753e2e854fa2cd983ab721b14bf69fb38ae1eeb3
sxabc <2947261028@qq.com>
2022-05-08 14:25:34 +0800

    feat: screenfull全屏功能

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -5,6 +5,7 @@
     <div class="right-menu">
     <theme-picker class="right-menu-item hover-effect"></theme-picker>
     <lang-select class="right-menu-item hover-effect" />
+    <screenfull class="right-menu-item hover-effect" />
       <!-- 头像 -->
       <el-dropdown class="avatar-container" trigger="click">
         <div class="avatar-wrapper">
@@ -37,6 +38,7 @@ import { ref, watch } from 'vue'
 import { useRoute } from 'vue-router'
 import LangSelect from '@/components/LangSelect'
 import ThemePicker from '@/components/ThemeSelect/index'
+import Screenfull from '@/components/Screenfull'
 
 const store = useStore()
 const logout = () => {
