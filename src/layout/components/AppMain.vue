b8021a42df21f2226fb04611a143796369540468
sxabc <2947261028@qq.com>
2022-05-08 21:14:06 +0800

    feat: contextMenu展示处理

--- src/layout/components/AppMain.vue
+++ src/layout/components/AppMain.vue
@@ -7,9 +7,24 @@
 <script setup>
 import { watch } from 'vue'
 import { isTags } from '@/utils/tags'
-import { generateTitle } from '@/utils/i18n'
 import { useRoute } from 'vue-router'
 import { useStore } from 'vuex'
+import { generateTitle, watchSwitchLang } from '@/utils/i18n'
+
+/**
+ * 国际化 tags
+ */
+watchSwitchLang(() => {
+  store.getters.tagsViewList.forEach((route, index) => {
+    store.commit('app/changeTagsView', {
+      index,
+      tag: {
+        ...route,
+        title: getTitle(route)
+      }
+    })
+  })
+})
 
 const route = useRoute()
 
---
+++ src/views/error-page/401.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>aaa</div>
+</template>
+
+<script setup>
+
+</script>
