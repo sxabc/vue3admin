e5ded605dab2feab94812035efcc92770b816f81
sxabc <2947261028@qq.com>
2022-05-09 11:06:12 +0800

    feat: 处理接口国际化问题

--- src/views/profile/index.vue
+++ src/views/profile/index.vue
@@ -30,6 +30,7 @@ import Feature from './components/Feature.vue'
 import Author from './components/Author.vue'
 import { ref } from 'vue'
 import { getFeature } from '@/api/user'
+import { watchSwitchLang } from '@/utils/i18n'
 
 const activeName = ref('feature')
 
@@ -38,6 +39,9 @@ const getFeatureData = async () => {
   featureData.value = await getFeature()
 }
 getFeatureData()
+
+// 监听语言切换
+watchSwitchLang(getFeatureData)
 </script>
 
 <style lang="scss" scoped>
